baf3001d94ca4f914ecd66d3a126e35e598d8bda
Wen Tao <software.wentao@gmail.com>
2022-03-20 19:04:08 +0800

    完成text processing

---
+++ text/src/bin/fromstr-trait.rs
@@ -0,0 +1,45 @@
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq)]
+struct RGB {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+impl FromStr for RGB {
+    type Err = std::num::ParseIntError;
+
+    fn from_str(hex_code: &str) -> Result<Self, Self::Err> {
+        let r: u8 = u8::from_str_radix(&hex_code[1..3], 16)?;
+        let g: u8 = u8::from_str_radix(&hex_code[3..5], 16)?;
+        let b: u8 = u8::from_str_radix(&hex_code[5..7], 16)?;
+
+        Ok(RGB { r, g, b })
+    }
+}
+
+fn main() {
+    let code: &str = &r"#fa7268";
+    match RGB::from_str(code) {
+        Ok(rgb) => {
+            println!(
+                r"The RGB color code is: R: {} G: {} B: {}",
+                rgb.r, rgb.g, rgb.b
+            );
+        }
+        Err(_) => {
+            println!("{} is not a valid color hex code!", code);
+        }
+    }
+
+    // test whether from_str performs as expected
+    assert_eq!(
+        RGB::from_str(&r"#fa7268").unwrap(),
+        RGB {
+            r: 250,
+            g: 114,
+            b: 104
+        }
+    );
+}
---
+++ text/src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("Chapter 18 Text Processing");
+}
